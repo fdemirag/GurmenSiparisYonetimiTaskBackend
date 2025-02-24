a0bae390e90c888ecaaa945f90df4f96f8d12bfb
fdemirag <f2000fatih@hotmail.com>
2025-02-25 01:01:24 +0300

    Veri tabanı bağlantısı hazırlandı.

---
+++ DataAccess/Contexts/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace DataAccess
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("PostgresConnection"); // PostgreSQL bağlantı stringi
+        }
+
+        // PostgreSQL bağlantısı oluşturma
+        public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
+    }
+}
---
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace DataAccess
+{
+    public static class DataAccessServiceRegistration
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+        {
+
+
+            services.AddScoped<DapperContext>();  // DapperContext servisini ekliyoruz
+
+            //// Autofac ile DI container'ı entegre etmek için
+            //var builder = new ContainerBuilder();
+
+            //// "Repository" ile biten tüm sınıfları ekle
+            //builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            //       .Where(x => x.Name.EndsWith("Repository"))
+            //       .AsImplementedInterfaces()
+            //       .InstancePerLifetimeScope();
+
+            //// Autofac'in servisleri kullanabilmesi için IServiceCollection'a ekle
+            //builder.Populate(services);
+
+            return services;
+        }
+    }
+}
