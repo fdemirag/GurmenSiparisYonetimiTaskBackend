0fbd5ec9f403b5763c8b16e0fc6cf973670a84d7
fdemirag <f2000fatih@hotmail.com>
2025-02-25 16:21:38 +0300

    CustomerDTO'ya Id eklendi

--- Business/DTOs/Customer/CustomerDTO.cs
+++ Business/DTOs/Customer/CustomerDTO.cs
@@ -8,7 +8,7 @@ namespace Business.DTOs.Customer
 {
     public class CustomerDTO
     {
-       
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
